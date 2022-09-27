1f54ccc3e9d031f9a2c102ab2f7ca67ba4d8e73c
Jwicaksana <jwicaksana@connect.ust.hk>
2022-09-27 17:19:04 +0800

    retouch

--- helper.py
+++ helper.py
@@ -1,4 +1,5 @@
 import torch
+import torchvision
 import torchvision.transforms.functional as TF
 import matplotlib.pyplot as plt
 import os
@@ -33,7 +34,7 @@ colors = ['r', 'g', 'b', 'c', 'k', 'y','m', 'c']
 
 class retouch(Dataset):
     def __init__(self, data_path, train=False, \
-                IMAGE_SIZE=(224,224), CROP_SIZE=(200,200), num_class = 3, noisy=False):
+                IMAGE_SIZE=(224,224), CROP_SIZE=(200,200), num_class = 3, noisy=True):
         self.data = data_path
         self.train = train
         self.IMAGE_SIZE = IMAGE_SIZE
@@ -112,7 +113,7 @@ class retouch(Dataset):
 class Cancer(Dataset):
     def __init__(self, im_path, mask_path, train=False, \
                 IMAGE_SIZE=(256,256), CROP_SIZE=(224,224), 
-                noisy=False):
+                noisy=True):
         self.data = im_path
         self.label = mask_path
         self.train = train
@@ -156,9 +157,9 @@ class Cancer(Dataset):
         bbox_mask = torch.zeros(y.shape)
         if torch.sum(y) > 0:
             _, w, h = torch.where(y == 1)
-            # should be within the image size, eg 0-224
+            # should be within the image size,
             if self.noisy:
-                margin = torch.randint(0, 10, (4,))
+                margin = torch.randint(1, 10, (4,))
             else:
                 margin = torch.zeros((4,))
             w_min, w_max, h_min, h_max = torch.min(w)-margin[0], \
@@ -174,7 +175,7 @@ class Cancer(Dataset):
 
 
 #####################################
-# Dataloader : Cancer_v2
+# Dataloader : Cancer_v2 or retouch_v2
 # Purpose : select pseudo labels 
 #####################################
 class cancer_v2(Dataset):
@@ -189,6 +190,19 @@ class cancer_v2(Dataset):
     def __getitem__(self, idx):
         x,y1,y2 = self.im_store[idx], self.pl1_store[idx], self.pl2_store[idx]
         return x, y1, y2
+
+class retouch_v2(Dataset):
+    def __init__(self, im_store, pl1_store, pl2_store):
+        self.im_store = im_store
+        self.pl1_store = pl1_store
+        self.pl2_store = pl2_store
+        
+    def __len__(self):
+        return len(self.im_store)
+    
+    def __getitem__(self, idx):
+        x,y1,y2 = self.im_store[idx], self.pl1_store[idx], self.pl2_store[idx]
+        return x, y1, y2, y2 # second y2 as placeholder
 
 
 ############################################
@@ -260,7 +274,7 @@ def rand_bbox(size, lam):
 #        : bbox and image indicates the type of labels available
 # outputs: the dice score of target data and the ratio of data selected [trust]
 ##########################################
-def select_pl(nets_1, nets_2, device, trainloader, im_store, \
+def select_pl_singleclass(nets_1, nets_2, device, trainloader, im_store, \
     pl1_store, pl2_store, \
     TH = 0.9, bbox=False, image=False):
     counter, dice_acc = 0,0 # create variable to store the num data and accuracy
@@ -274,26 +288,88 @@ def select_pl(nets_1, nets_2, device, trainloader, im_store, \
             y_pred, y2_pred = nets_1(imgs_cuda1), nets_2(imgs_cuda2)
             y_pred, y2_pred = torch.sigmoid(y_pred), torch.sigmoid(y2_pred)
             y_pred, y2_pred = (y_pred > 0.5).float(), (y2_pred > 0.5).float()
-
-            dice_net12  = dice_coeff(y2_pred, y_pred)
+            
+            if bbox:
+                bbox_gt = bbox_gt.to(device)
+                y_pred_ = y_pred * bbox_gt
+                y2_pred_ = y2_pred * bbox_gt
+            
+            if bbox:
+                dice_net12  = dice_coeff(y2_pred_, y_pred_)
+            else:
+                dice_net12  = dice_coeff(y2_pred, y_pred)
+                
             dice_wrt_gt = dice_coeff(masks.type(torch.float).to(device), y_pred)
 
             if dice_net12 >= TH:
                 dice_acc += dice_wrt_gt
-                counter += 1
+                if bbox:
+                    dice_bbox = dice_coeff(y_pred_, bbox_gt)
+                    if dice_bbox < 0.5:
+                        continue
                 im_store.append(imgs[0])
+                counter+=1
                 if bbox: #refine predictions if bbox
-                    bbox_gt = bbox_gt.to(device)
-                    y_pred = y_pred * bbox_gt
-                    y2_pred = y2_pred * bbox_gt
+                    y_pred = y_pred_
+                    y2_pred = y2_pred_
                 pl1_store.append(y_pred[0].detach().cpu())
                 pl2_store.append(y2_pred[0].detach().cpu())
         
     # return the counter per total length and dice acc for evaluation
-    try:
-        return (dice_acc/counter, counter/len(trainloader))
-    except:
-        return (0,0)
+    return counter
+
+
+def select_pl_multiclasses(nets_1, nets_2, device, trainloader, im_store, \
+    pl1_store, pl2_store, \
+    TH = 0.9, bbox=False, image=False):
+    counter, dice_acc = 0,0 # create variable to store the num data and accuracy
+    nets_1.eval()
+    nets_2.eval()
+    with torch.no_grad():
+        # trainloader contains the actual label but is not used # 
+        for (imgs, masks, bbox_gt, mask_gt) in trainloader:  
+            # bbox_gt, masks, mask_gt dim , (batch, CLASSES [3], W, H)
+            imgs_cuda1, imgs_cuda2 = imgs.to(device), imgs.to(device)
+
+            y_pred, y2_pred = nets_1(imgs_cuda1), nets_2(imgs_cuda2)
+            y_pred, y2_pred = torch.sigmoid(y_pred), torch.sigmoid(y2_pred)
+            y_pred, y2_pred = (y_pred > 0.5).float(), (y2_pred > 0.5).float()
+            
+            if image:
+                mask_gt = mask_gt.to(device)
+                y_pred_ = y_pred * mask_gt
+                y2_pred_ = y2_pred * mask_gt
+            
+            if bbox:
+                bbox_gt = bbox_gt.to(device)
+                y_pred_ = y_pred * bbox_gt
+                y2_pred_ = y2_pred * bbox_gt
+            
+            if bbox or image:
+                dice_net12  = dice_coeff(y2_pred_, y_pred_)
+            else:
+                dice_net12  = dice_coeff(y2_pred, y_pred)
+                
+            dice_wrt_gt = dice_coeff(masks.type(torch.float).to(device), y_pred)
+
+            if dice_net12 >= TH:
+                dice_acc += dice_wrt_gt
+                if bbox:
+                    dice_bbox = dice_coeff(y_pred_, bbox_gt)
+                    if dice_bbox < 0.5:
+                        continue
+
+                im_store.append(imgs[0])
+                counter+=1
+                if bbox or image: #refine predictions
+                    y_pred = y_pred_
+                    y2_pred = y2_pred_ 
+
+                pl1_store.append(y_pred[0].detach().cpu())
+                pl2_store.append(y2_pred[0].detach().cpu())
+        
+    # return the counter per total length and dice acc for evaluation
+    return counter
 
 ###########################
 ## Test the network acc ###
@@ -322,6 +398,37 @@ def test(epoch, testloader, net, device, acc=None, loss=None):
    
     del t_acc, t_loss
 
+def test_multiclasses(epoch, testloader, net, device, acc=None, loss=None, num_client=3):
+    net.eval()
+    t_loss, t_acc = 0,0
+    with torch.no_grad():
+        for (imgs, masks, _,_) in testloader:
+            masks = masks.type(torch.float32)
+            imgs, masks = imgs.to(device), masks.to(device)
+            
+            ###########################################
+            masks_pred = net(imgs)
+            masks_pred = torch.sigmoid(masks_pred)
+            l_ = 1 - dice_coeff(masks_pred, masks.type(torch.float))
+            t_loss += l_.item()
+            #######################################################
+            masks_pred = (masks_pred>0.5).float()
+            '''
+            multiclasses
+            '''
+            t_acc_network1 = 0.0
+            for i in range(num_client):
+                t_acc_network1 += dice_coeff(masks[:,i].type(torch.float), \
+                                            masks_pred[:,i]).item()
+            t_acc_network1 /= 3.0
+            t_acc += t_acc_network1
+    
+    if acc is not None:
+        acc.append(t_acc / len(testloader))
+    if loss is not None:
+        loss.append(t_loss/ len(testloader))
+   
+    del t_acc, t_loss
 
 
 '''
@@ -381,6 +488,77 @@ def train_model(trainloader, net_stu, optimizer_stu, \
         t_loss += l_.item()
         masks_stu = (masks_stu.detach() > 0.5).float()
         t_acc_network = dice_coeff(masks_stu, masks.type(torch.float)).item()
+        t_acc += t_acc_network
+                
+        
+    if acc is not None:
+        try:
+            acc.append(t_acc/len(trainloader))
+        except:
+            acc.append(0.0)
+    if loss is not None:
+        try:
+            loss.append(t_loss/len(trainloader))
+        except:
+            loss.append(0.0)
+
+def train_model_multiclasses(trainloader, net_stu, optimizer_stu, \
+                     device, acc=None, loss = None, supervision_type='labeled', \
+                     warmup=False, CE_LOSS= None, TH=0.9, FedMix_network=1, num_client=3):
+    net_stu.train()
+    t_loss, t_acc = 0,0    
+    labeled_len = len(trainloader)
+    labeled_iter = iter(trainloader)
+    for _ in range(labeled_len):
+        imgs, masks, y_pl, _ = next(labeled_iter)
+        imgs, masks = imgs.to(device), masks.to(device)
+        optimizer_stu.zero_grad()
+        ###################################################
+        l_ = 0
+        ## get the prediction from the model of interest ##
+        masks_stu = torch.sigmoid(net_stu(imgs))
+        ### if supervision type is labeled, just train as normal with dice ###
+        if supervision_type == 'labeled':
+            dc = dice_coeff(masks_stu, masks.type(torch.float))
+            l_stu = (1 - dc)[0]
+            l_ = l_stu
+        else:
+            if warmup:
+                CE_LOSS = CE_LOSS.to(device) 
+                # to augment #
+                gamma_v = (torch.rand(1)*2)
+                gamma_v = gamma_v.to(device)
+                imgs_augmented = intensity_shift(imgs, gamma=gamma_v)
+                # find confident pixels for training
+                mask = (masks_stu>TH) + (masks_stu<(1-TH))
+                mask = mask.detach()
+                masks_stu = (masks_stu.detach() > 0.5).float()
+
+                masks_augmented = torch.sigmoid(net_stu(imgs_augmented))
+                l_stu = CE_LOSS(masks_augmented[mask], masks_stu[mask])
+                l_ = l_stu
+
+            else: # for non FedST
+                if FedMix_network == 1:
+                    masks_teach = y_pl.to(device)
+                else:
+                    masks_teach = masks.to(device)
+
+                l_stu = (1 - dice_coeff(masks_stu, masks_teach.type(torch.float)))[0]
+                l_ = l_stu
+        #############################
+        l_.backward()
+        optimizer_stu.step()
+
+        # for evaluation 
+        t_loss += l_.item()
+        masks_stu = (masks_stu.detach() > 0.5).float()
+        dc = 0.0
+        for i in range(num_client):
+            dc = dc + dice_coeff(masks_stu[:,i], masks[:,i].type(torch.float))
+        dc = dc/3.0
+        
+        t_acc_network = dc.item()
         t_acc += t_acc_network
                 
         
